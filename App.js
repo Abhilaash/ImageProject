6c7526da89a76e10bfcd2ecb50e36f8d72676198
Abhilaash <abhilaash.velamati@gmail.com>
2018-07-05 10:14:47 -0400

    Efficiency Improvements
    Uses props for the lorem ipsum text

--- App.js
+++ App.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {FlatList, StyleSheet, Text, ScrollView, View } from 'react-native';
+
+class Service extends React.Component {
+  render() {
+    return (
+      <Text style={{textAlign: 'center'}}>
+        {this.props.service} Lorem ipsum dolor sit amet, dicam ubique intellegat eos in, te laoreet probatus facilisis sed. Vim ex dolore omnesque. An simul similique vix, per ad menandri indoctum scriptorem. Vide sonet blandit mel ad, ea timeam patrioque mei. Qui at doming adipiscing.
+      </Text>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ScrollView style={styles.container}>
+          <Text style={{flex: 3, textAlign: 'center', height: 600}}>
+            Camera
+          </Text>
+          <View style = {{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
+            <View style = {{backgroundColor: 'powderblue'}}>
+              <Service service = "AWS"  />
+            </View>
+            <View style = {{backgroundColor: 'skyblue'}}>
+              <Service service = "Azure"/>
+            </View>
+            <View style = {{backgroundColor: 'steelblue'}}>
+              <Service service = "Google Cloud" />
+            </View>
+          </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 2,
+    paddingTop: 22,
+    paddingBottom: 22,
+  },
+
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 10,
   },
 });
