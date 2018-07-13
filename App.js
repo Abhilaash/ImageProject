655082c4852b5d9e5b4d65195a1cd8da2222765d
Abhilaash <abhilaash.velamati@gmail.com>
2018-07-13 10:34:41 -0400

    Camera works
    Camera still needs dimension fixes

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {FlatList, StyleSheet, Text, ScrollView, View } from 'react-native';
+import {FlatList, StyleSheet, Text, TouchableOpacity, ScrollView, View } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 class Service extends React.Component {
   render() {
@@ -11,13 +12,51 @@ class Service extends React.Component {
   }
 }
 
-export default class App extends React.Component {
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    type: Camera.Constants.Type.back,
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
   render() {
-    return (
-      <ScrollView style={styles.container}>
-          <Text style={{flex: 3, textAlign: 'center', height: 600}}>
-            Camera
-          </Text>
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <ScrollView style={styles.container}>
+          <Camera style={{flex: 4, height: 600}} type={this.state.type}>
+            <View
+              style={{
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                    ? Camera.Constants.Type.front
+                    : Camera.Constants.Type.back,
+                  });
+                }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
           <View style = {{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
             <View style = {{backgroundColor: 'powderblue'}}>
               <Service service = "AWS"  />
@@ -29,15 +68,16 @@ export default class App extends React.Component {
               <Service service = "Google Cloud" />
             </View>
           </View>
-      </ScrollView>
-    );
+        </ScrollView>
+      );
+    }
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 2,
-    paddingTop: 22,
-    paddingBottom: 22,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 2,
+      paddingTop: 22,
+      paddingBottom: 22,
+    },
+  });
