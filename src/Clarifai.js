3c6a4d0f3f660807208b6259f64d00ed98720391
Abhilaash <abhilaash.velamati@gmail.com>
2018-08-14 16:07:59 -0400

    Changed to Clarifai

---
+++ src/Clarifai.js
@@ -0,0 +1,18 @@
+async function clarifaiAnalysisAsync(bytes) {
+  try{
+    let response = await fetch('http://18.207.234.121:8080/api/getlabels_goog', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        image: bytes
+      })
+    });
+    let responseJson = await response.json();
+    return responseJson.message;
+    } catch (error) {
+    console.log("error section...")
+    console.error(error, response);
+  }
+}
+
+export {clarifaiAnalysisAsync};
